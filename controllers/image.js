75b662a68b346915414977ee605ade5b820af7ab
gunaerode <guna.learn2016@gmail.com>
2019-09-02 01:48:04 +0530

    learn(docker): Key removed & ingnore package-lock.json

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '05a898ab1327491383ba4e45e11826b5'
+ apiKey: 'YOUR_API_KEY_HERE'
 });
 
 const handleApiCall = (req, res) => {
